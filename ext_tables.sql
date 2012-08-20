c71a230538094f86f6d31b7d126cb6c0dbf59942
Steffen Müller <typo3@t3node.com>
2012-08-20 20:58:18 +0200

    Added required fields to sys_log table
    Change-Id: I4fac7c402a5cb82d2cea54486ec16e4f88ec58fb

---
+++ ext_tables.sql
@@ -0,0 +1,12 @@
+#
+# Table structure for table 'sys_log'
+#
+CREATE TABLE sys_log (
+  request_id varchar(13) DEFAULT '' NOT NULL,
+  time_micro float DEFAULT '0' NOT NULL,
+  component varchar(255) DEFAULT '' NOT NULL,
+  level tinyint(1) unsigned DEFAULT '0' NOT NULL,
+  message text,
+  data text,
+  KEY request (request_id)
+);
